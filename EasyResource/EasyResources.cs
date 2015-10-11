9f0a60bd996f9b7c9539c735fe5f7600148011d0
David Walker <github-dwalker@vorteon.com>
2015-10-11 07:48:08 -0400

    Initial checkin

---
+++ EasyResource/EasyResources.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grax.EasyResource
+{
+    public static class EasyResources
+    {
+        /// <summary>
+        /// Get embedded resource from the assembly that contains type T
+        /// </summary>
+        /// <typeparam name="T">Any type from the assembly that contains the embedded resource.</typeparam>
+        /// <param name="resourceName">Path name to embedded resource or full name from manifest resource name</param>
+        /// <returns></returns>
+        public static string GetStringResource<T>(string resourceName)
+        {
+            return ResourceFromType<T>.GetStringResource(resourceName);
+        }
+
+        /// <summary>
+        /// Get embedded resource from the assembly that contains type T
+        /// </summary>
+        /// <typeparam name="T">Any type from the assembly that contains the embedded resource.</typeparam>
+        /// <param name="resourceName">Path name to embedded resource or full name from manifest resource name</param>
+        /// <returns></returns>
+        public static byte[] GetResource<T>(string resourceName)
+        {
+            return ResourceFromType<T>.GetResource(resourceName);
+        }
+
+        /// <summary>
+        /// Get embedded resource from the assembly
+        /// </summary>
+        /// <param name="assembly">The assembly that contains the embedded resource</param>
+        /// <param name="resourceName">Path name to embedded resource or full name from manifest resource name</param>
+        /// <returns></returns>
+        public static string GetStringResource(this Assembly assembly, string resourceName)
+        {
+            return
+                assembly.FetchStringResource(assembly.GetMatchingResourceName(resourceName));
+        }
+
+        /// <summary>
+        /// Get embedded resource from the assembly
+        /// </summary>
+        /// <param name="assembly">The assembly that contains the embedded resource</param>
+        /// <param name="resourceName">Path name to embedded resource or full name from manifest resource name</param>
+        /// <returns></returns>
+        public static byte[] GetResource(this Assembly assembly, string resourceName)
+        {
+
+            return
+                assembly.FetchResource(assembly.GetMatchingResourceName(resourceName));
+        }
+
+        private static string FetchStringResource(this Assembly resourceAssembly, string resourceName)
+        {
+
+            using (Stream stream = resourceAssembly.GetManifestResourceStream(resourceName))
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+
+        public static byte[] FetchResource(this Assembly resourceAssembly, string resourceName)
+        {
+            using (Stream stream = resourceAssembly.GetManifestResourceStream(resourceName))
+            {
+                var length = (int)stream.Length;
+                var returnValue = new byte[length];
+
+                stream.Read(returnValue, 0, length);
+
+                return returnValue;
+            }
+        }
+
+        private static string GetMatchingResourceName(this Assembly assembly, string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("name parameter must not be null or empty", "name");
+            }
+            name = name.Replace("\\", ".").Replace("/", ".");
+            return assembly.GetManifestResourceNames().Single(v => v.EndsWith(name, StringComparison.CurrentCultureIgnoreCase));
+        }
+
+        internal static class ResourceFromType<T>
+        {
+            static Assembly resourceAssembly = typeof(T).GetTypeInfo().Assembly;
+            static string[] names = resourceAssembly.GetManifestResourceNames();
+
+            private static string GetMatchingResourceName(string name)
+            {
+                if (string.IsNullOrWhiteSpace(name))
+                {
+                    throw new ArgumentException("name parameter must not be null or empty", "name");
+                }
+                name = name.Replace("\\", ".").Replace("/", ".");
+                return names.Single(v => v.EndsWith(name, StringComparison.CurrentCultureIgnoreCase));
+            }
+
+            public static string GetStringResource(string name)
+            {
+                name = GetMatchingResourceName(name);
+                return FetchStringResource(resourceAssembly, name);
+            }
+
+            public static byte[] GetResource(string name)
+            {
+                name = GetMatchingResourceName(name);
+                return FetchResource(resourceAssembly, name);
+            }
+        }
+    }
+}
---
+++ EasyResourceTests/UnitTest1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Reflection;
+using Grax.EasyResource;
+
+namespace EasyResourceTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GetStringResourceTest()
+        {
+            string resource1 = EasyResources.GetStringResource<UnitTest1>("\\Resources\\textfile.txt");
+            string resource2 = EasyResources.GetStringResource<UnitTest1>("/Resources/textfile.txt");
+            string resource3 = EasyResources.GetStringResource<UnitTest1>("EasyResourceTestsNamespace.Resources.textfile.txt");
+
+            Assert.AreEqual("This is a test text file.", resource1);
+            Assert.AreEqual("This is a test text file.", resource2);
+            Assert.AreEqual("This is a test text file.", resource3);
+        }
+
+        [TestMethod]
+        public void GetBinaryResourceTest()
+        {
+            byte[] startsWithExpected = { 137, 80, 78, 71, 13, 10, 26, 10, 0, 0 };
+            byte[] endsWithExpected = { 0, 0, 73, 69, 78, 68, 174, 66, 96, 130 };
+
+            byte[] resource = EasyResources.GetResource<UnitTest1>("/Resources/Image.png");
+            Assert.AreEqual(42956, resource.Length);
+
+            var startsWith = resource.Take(10).ToArray();
+            var endsWith = resource.Skip(resource.Length - 10).Take(10).ToArray();
+
+            Assert.IsTrue(startsWithExpected.SequenceEqual(startsWith));
+            Assert.IsTrue(endsWithExpected.SequenceEqual(endsWith));
+        }
+
+        [TestMethod]
+        public void GetAssemblyStringResourceTest()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            string resource1 = assembly.GetStringResource("\\Resources\\textfile.txt");
+            string resource2 = assembly.GetStringResource("/Resources/textfile.txt");
+            string resource3 = assembly.GetStringResource("EasyResourceTestsNamespace.Resources.textfile.txt");
+
+            Assert.AreEqual("This is a test text file.", resource1);
+            Assert.AreEqual("This is a test text file.", resource2);
+            Assert.AreEqual("This is a test text file.", resource3);
+        }
+
+        [TestMethod]
+        public void GetAssemblyBinaryResourceTest()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            byte[] startsWithExpected = { 137, 80, 78, 71, 13, 10, 26, 10, 0, 0 };
+            byte[] endsWithExpected = { 0, 0, 73, 69, 78, 68, 174, 66, 96, 130 };
+
+            byte[] resource = assembly.GetResource("/Resources/Image.png");
+            Assert.AreEqual(42956, resource.Length);
+
+            var startsWith = resource.Take(10).ToArray();
+            var endsWith = resource.Skip(resource.Length - 10).Take(10).ToArray();
+
+            Assert.IsTrue(startsWithExpected.SequenceEqual(startsWith));
+            Assert.IsTrue(endsWithExpected.SequenceEqual(endsWith));
+        }
+    }
+}
